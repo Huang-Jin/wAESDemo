c5796aec5145e7723d51ebf9c5ce5c0f774f64e6
Huang-Jin <1309740625@qq.com>
2017-11-03 18:13:08 +0800

    AES Over

--- wAESDemo/wAESDemo.cpp
+++ wAESDemo/wAESDemo.cpp
@@ -1,14 +1,20 @@
-
 #include "wAES.h"
 
 int main(int argc, char** argv)
 {
 	WAES aesDemo;
 
-	char data[] = "abcdefghijklmnop";
-	char key[] = "ponmlkjihgfedcba";
+	//char data[] = "abcdefghijklmnop";
+	//char key[] = "ponmlkjihgfedcba";
 
-	char *result = aesDemo.encrypt(data, 16, key);
+	//aesDemo.encrypt(data, 16, key);
+	//aesDemo.decrypt(data, 16, key);
+
+	char filename[] = "code.txt";
+	char codedfilename[] = "code.wAESe";
+	char key[] = "ponmlkjihgfedcba";
+	aesDemo.encryptFile(filename, key);
+	aesDemo.decryptFile(codedfilename, key);
 
 	return 0;
 }
